7eda92695946d1797dbb43e8f5d138f4a64fdfc7
Eric <dengyangself@163.com>
2017-10-25 17:06:39 +0800

    update reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,9 +6,16 @@ import { createStore, renderDevTools } from '../utils/devTools';
 
 import FriendListApp from './FriendListApp';  
 import * as reducers from '../reducers';
+import {addFriend, deleteFriend, starFriend} from '../actions/FriendsActions';
 
 const reducer = combineReducers(reducers);
 const store = createStore(reducer);
+
+store.dispatch(addFriend('Barack Obama'));
+
+store.dispatch(deleteFriend(1));
+
+store.dispatch(starFriend(4));
 
 export default class App extends Component {
   render() {
