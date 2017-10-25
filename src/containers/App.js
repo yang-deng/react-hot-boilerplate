311bb08a502e34f3874e426878797b62bb1d9b61
Eric <dengyangself@163.com>
2017-10-25 16:41:05 +0800

    add app container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';  
+import { combineReducers } from 'redux';  
+import { Provider } from 'react-redux';
+
+import { createStore, renderDevTools } from '../utils/devTools';
+
+import FriendListApp from './FriendListApp';  
+import * as reducers from '../reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <Provider store={store}>
+          {() => <FriendListApp />}
+        </Provider>
+      </div>
     );
   }
 }
