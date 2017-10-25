9dd63ba78fb147d7dada7ee6db60e5a473bc5db1
Eric <dengyangself@163.com>
2017-10-25 17:14:31 +0800

    update component

---
+++ src/containers/FriendListApp.js
@@ -0,0 +1,32 @@
+import { bindActionCreators } from 'redux';  
+import { connect } from 'react-redux';  
+import * as FriendsActions from '../actions/FriendsActions';  
+import { FriendList, AddFriendInput } from '../components';
+
+@connect(state => ({
+    friendlist: state.friendlist
+}))
+export default class FriendListApp extends Component {
+    static propTypes = {
+        friendsById: PropTypes.object.isRequired,
+        dispatch: PropTypes.func.isRequired
+    }
+
+    render() {
+        const {
+            friendlist: {
+                friendsById
+            },
+            dispatch
+        } = this.props;
+        const actions = bindActionCreators(FriendsActions, dispatch);
+
+        return (
+            <div className={styles.friendListApp}>
+              <h1>The FriendList</h1>
+              <AddFriendInput addFriend={actions.addFriend} />
+              <FriendList friends={friendsById} actions={actions} />
+            </div>
+        );
+    }
+}
